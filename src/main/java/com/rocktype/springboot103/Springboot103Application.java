143c1b89a5b571ba9eac91930e3c7cde428c7c15
Ryan Murphy <ryanmurphy2014@gmail.com>
2020-03-02 20:55:29 -0500

    Initial commit

---
+++ src/main/java/com/rocktype/springboot103/Springboot103Application.java
@@ -0,0 +1,13 @@
+package com.rocktype.springboot103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot103Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rocktype/springboot103/Springboot103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocktype.springboot103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
